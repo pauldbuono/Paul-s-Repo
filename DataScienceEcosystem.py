8cb9a4b7df3ae60092412b9f2ca1ebc0a1d57715
Paul Buono <124936161+pauldbuono@users.noreply.github.com>
2023-03-05 01:07:23 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem #
+
+# This notebook provides a summary of data science tools and ecosystem.
+# 
+
+# <ol>
+#   <p>Some of the popular languages that Data Scientists use are:</p>
+#   <li>Python</li>
+#   <li>R</li>
+#   <li>Java</li>
+#   <li>SQL</li>
+# </ol>
+
+# <ul> <b> Objectives: </b>
+# <li>Languages of data science</li>
+# <li>Packages</li>
+# <li>Data science tools</li>
+# </ul>
+
+# Some of the commonly used libraries used by Data Scientists include:
+# <ol>
+#   <p>Some of the commonly used libraries used by Data Scientists include:</p>
+#   <li>Numpy</li>
+#   <li>TensorFlow</li>
+#   <li>Scipy</li>
+# </ol>
+
+# | Data Science Tools |
+# |----------|
+# | PyCharm |
+# | Visual Studio |
+# | Spyder |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[3]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+x=200
+hours=x/60
+print(hours)
+
+
+# ## Author
+# Paul Buono
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
